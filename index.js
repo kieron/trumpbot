44a6e6f28de1acf5765f03fc42aac6dc9956c99e
Kieron <kieronboz@gmail.com>
2023-06-27 12:29:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ client.on("messageCreate", (message) => antiSpam.message(message));
 
 const AntiSpam = require("discord-anti-spam");
 const antiSpam = new AntiSpam({
-  warnThreshold: 3, // Amount of messages sent in a row that will cause a warning.
+  warnThreshold: 3, // Amount of messages sent in a row that will cause a warning..
   kickThreshold: 7, // Amount of messages sent in a row that will cause a ban.
   banThreshold: 7, // Amount of messages sent in a row that will cause a ban.
   maxInterval: 2000, // Amount of time (in milliseconds) in which messages are considered spam.
